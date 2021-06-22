97d285e9184db1f8f9be2b574ac6075b9381a85a
yihanYozikua <newy860318@gmail.com>
2021-06-23 00:33:54 +0800

    modified the UI

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+let userOnline = document.getElementById("userOnline");
+let userList = document.getElementById("userList");
+let userListClose = document.getElementById("userListClose");
+
+userOnline.addEventListener('click', () => {
+  console.log("show users online");
+  // userList.style.display = "flex";
+  userList.style.right = 0;
+  userList.style.boxShadow = "2px 2px 30px 10px rgba(230, 230, 230, 0.2);";
+});
+
+userListClose.addEventListener('click', () => {
+  userList.style.right = "-250px";
+  // userList.style.display = "none";
+});
