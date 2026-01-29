1a188950d31a16c89f90eccea78293c160d8b403
hsiao.yihan <hsiao.yihan@lycorp.co.jp>
2026-01-29 23:23:14 +0900

    refactor

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,23 +1,104 @@
-const socket = io.connect('/');
+// public/js/chat.js
+// ES module
+const URL = '/';
+let socket = null;
 
-// Socket events
+// keep track of handlers so we can remove them if needed
+const _handlers = new Map();
 
-  // Whenever the server emits 'login', log the login message
-socket.on('newUser', (data) => {
-  // Display the welcome message
-  var message = "Welcome to Socket.IO Chat – ";
-  console.log(message)
-  console.log(data)
-});
+/**
+ * Connect (idempotent)
+ */
+function connect() {
+  if (!socket) {
+    socket = io(URL);
+  }
+  return socket;
+}
 
-socket.on('userLeave', (data) => {
-  const message = "Someone Leave~"
-  console.log(message)
-  console.log(data)
-})
+/**
+ * Disconnect and clear handlers
+ */
+function disconnect() {
+  if (!socket) {
+    return;
+  }
+  socket.disconnect();
+  socket = null;
+  _handlers.clear();
+}
 
-socket.on('disconnect', () => {
-  console.log('you have been disconnected');
-});
+/**
+ * Register a handler and keep a reference so we can off() later
+ * @param {string} event
+ * @param {function} cb
+ */
+function on(event, cb) {
+  connect().on(event, cb);
+  if (!_handlers.has(event)) _handlers.set(event, new Set());
+  _handlers.get(event).add(cb);
+}
 
-export default socket
+/**
+ * Register a once handler
+ */
+function once(event, cb) {
+  connect().once(event, cb);
+  // do not add to _handlers (it's once)
+}
+
+/**
+ * Remove a single handler or all handlers for an event
+ */
+function off(event, cb) {
+  if (!socket) return;
+  if (cb) {
+    socket.off(event, cb);
+    const s = _handlers.get(event);
+    if (s) {
+      s.delete(cb);
+      if (s.size === 0) _handlers.delete(event);
+    }
+  } else {
+    // remove all handlers for event
+    const s = _handlers.get(event);
+    if (s) {
+      for (const fn of s) {
+        socket.off(event, fn);
+      }
+      _handlers.delete(event);
+    }
+  }
+}
+
+/**
+ * Emit wrapper
+ */
+function emit(event, payload) {
+  connect().emit(event, payload);
+}
+
+/**
+ * Bulk register: accept an object { eventName: handlerFn, ... }
+ */
+function registerHandlers(obj = {}) {
+  Object.entries(obj).forEach(([event, fn]) => on(event, fn));
+}
+
+/**
+ * Utility to get raw socket if needed (but prefer the wrapper methods)
+ */
+function getRawSocket() {
+  return socket;
+}
+
+export default {
+  connect,
+  disconnect,
+  on,
+  once,
+  off,
+  emit,
+  registerHandlers,
+  getRawSocket
+};
--- public/js/welcomePage.js
+++ public/js/welcomePage.js
@@ -1,68 +1,57 @@
-import socket from './chat.js';
+import Chat from './chat.js';
 
-$(function(){
-  var nickname = sessionStorage.getItem('myNickname');
-  var roomId = window.location.pathname.split('/')[2] || 'general';
+$(function () {
+  const nickname = sessionStorage.getItem('myNickname') ||
+                   (prompt('To join the room, a nickname is required...') || '').trim();
 
-  if(!nickname){
-    nickname = prompt('To join the room, a nickname is required...');
-
-    if (!nickname || nickname.trim() === '') {
-      alert('A nickname is REQUIRED to join this video call!!!');
-      window.location.href = '/';
-      return;
-    }
-
-    sessionStorage.setItem('myNickname', nickname.trim());
+  if (!nickname) {
+    alert('A nickname is REQUIRED to join this video call!!!');
+    window.location.href = '/';
+    return;
   }
+  sessionStorage.setItem('myNickname', nickname);
+  const roomId = window.location.pathname.split('/')[2] || 'general';
 
-  socket.emit('new user', nickname);
-  socket.emit('join room', {
-    username: nickname,
-    room: roomId
-  });
+  // connect and emit
+  Chat.emit('new user', nickname);
+  Chat.emit('join room', {username: nickname, room: roomId});
 
   $('#enterPage').hide();
   $('#chatroom-content').show();
+  const $frmMessage = $('#send-message');
+  const $boxMessage = $('#message');
+  const $chat = $('#chat');
 
-  var $frmMessage = $('#send-message');
-  var $boxMessage = $('#message');
-  var $chat = $('#chat');
-
-  $frmMessage.submit(function(e){
-    e.preventDefault();
-    socket.emit('send message', $boxMessage.val().trim());
-    $boxMessage.val('');
-  });
-
-  socket.on('usernames', function(data){
-    var sb = ' ';
-    for(var d = 0; d < data.length; d++ ) {
-      console.log(data[d]);
-      sb += '<i class="fas fa-user-circle"></i>&nbsp' + data[d] + "<br />";
+  Chat.on('usernames', (data) => {
+    let sb = ' ';
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      sb += '<i class="fas fa-user-circle"></i>&nbsp' + data[i] + "<br />";
     }
     $('div#users').html(sb);
+  });
+  Chat.on('chat', (server, msg) => {
+    const now = new Date();
+    let datetime = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate();
+    datetime += ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
 
+    $chat.append(
+      "<i style='color: #9bffb2;'><b>" + msg + "</b> <p style='font-size: 1.5vh;'>" + datetime + "</p></i>");
+  });
+  Chat.on('new message', (data) => {
+    const now = new Date();
+    let datetime = (now.getMonth() + 1) + '/' + now.getDate();
+    datetime += ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+
+    $chat.append(
+      "<div class='msg-content'><b style='font-weight:normal; background-color: gray; padding: 0.5vh 1vh; border-radius: 5px;'>" +
+      data.nick + "</b>&nbsp" + data.msg + " <i style='font-size: 1.5vh; color: gray;'>&nbsp" + datetime +
+      "</i><br /><div>");
   });
 
-  socket.on('chat', function(server,msg){
-    
-    var now = new Date(); 
-    var datetime = now.getFullYear()+'/'+(now.getMonth()+1)+'/'+now.getDate(); 
-      datetime += ' '+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds(); 
-
-    $chat.append("<i style='color: #9bffb2;'><b>" + msg + "</b> <p style='font-size: 1.5vh;'>" + datetime + "</p></i>");
-  });
-
-  socket.on('new message', function(data){
-    console.log("new msg append");
-    var msg = data.msg;
-    var name = data.nick;
-
-    var now = new Date(); 
-    var datetime = (now.getMonth()+1)+'/'+now.getDate(); 
-    datetime += ' '+now.getHours()+ ':' +now.getMinutes()+':'+now.getSeconds(); 
-
-    $chat.append("<div class='msg-content'><b style='font-weight:normal; background-color: gray; padding: 0.5vh 1vh; border-radius: 5px;'>" + name + "</b>&nbsp" + msg + " <i style='font-size: 1.5vh; color: gray;'>&nbsp" + datetime + "</i><br /><div>");
+  $frmMessage.submit(function (e) {
+    e.preventDefault();
+    Chat.emit('send message', $boxMessage.val().trim());
+    $boxMessage.val('');
   });
 });
